715d2bdc725f462e9d803c099b8e216a1d863a7c
unknown <yagamanikanta777@gmail.com>
2023-12-18 19:24:15 +0530

    hello-world

--- hell-world.sh
+++ hell-world.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-echo "Hello World, I am learning DevOps with AWS"
-PERSON1=mani
-PERSON2=vijaya
-
-echo "$PERSON1: Hi $PERSON2, Good Morning"
-echo "$PERSON2: Good Morning $PERSON1"
-echo "$PERSON1: How are you $PERSON2"
-echo "$PERSON2: i am good, how are you $PERSON1"
+DATE $(date)
+echo "the date is:$DATE"
