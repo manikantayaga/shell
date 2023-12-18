9193a9518ae8ad730949b69beda18054722131fe
unknown <yagamanikanta777@gmail.com>
2023-12-18 22:35:18 +0530

    hello-world

--- 7-conditions.sh
+++ 7-conditions.sh
@@ -1,9 +1,9 @@
 #!bin/bash
 NUMBER=$1
 
-if [$NUMBER -gt 10]
+if [ $NUMBER -gt 10 ]
 then
-  echo "$NUMBER is greater than 10"
+    echo "$NUMBER is greater than 10"
 else
-  echo  "$NUMBER not is greater than 10"
+    echo "$NUMBER is not greater than 10"
 fi
